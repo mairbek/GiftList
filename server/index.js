54eb9829bf68c91ed897631fe3d6379ceb66d3fa
Mairbek Khadikov <mairbek@conduit.xyz>
2024-06-25 15:53:20 -0700

    implemented

--- server/index.js
+++ server/index.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const MerkleTree = require('../utils/MerkleTree');
+const niceList = require('../utils/niceList.json');
+
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
+// create the merkle tree for the whole nice list
+const merkleTree = new MerkleTree(niceList);
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = merkleTree.getRoot();
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
+  console.log(body)
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  if (verifyProof(body.proof, body.name, MERKLE_ROOT)) {
     res.send("You got a toy robot!");
   }
   else {
